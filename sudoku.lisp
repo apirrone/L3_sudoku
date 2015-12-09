cfd487a4a79c4f3f7170388acbe73d45fa7cd2f2
antoine <antoine@localhost.localdomain>
2015-12-09 17:03:07 +0100

    Detection des inputs impossibles terminé

--- sudoku.lisp
+++ sudoku.lisp
@@ -3,9 +3,12 @@
 (defconstant empty 0)
 
 (defvar grid ())
+(defvar g-temp ())
 (defvar col)
 (defvar row)
 (defvar val)
+(defvar tmpList)
+(defvar nb)
 
 ;;Par soucis de practicité lors des entrées,
 ;; on associe aux colonnes une lettre
@@ -32,53 +35,86 @@
                  (5 0 0 0 0 0 0 0 2)
                  (0 0 7 2 0 6 9 0 0)
                  (0 4 0 5 0 8 0 7 0)))
-  (game-loop)
-
-)
-
-(defun game-loop()
+  (setq g-temp #2a((0 6 0 1 0 4 0 5 0)
+                 (0 0 8 3 0 5 6 0 0)
+                 (2 0 0 0 0 0 0 0 1)
+                 (8 0 0 4 0 7 0 0 6)
+                 (0 0 6 0 0 0 3 0 0)
+                 (7 0 0 9 0 1 0 0 4)
+                 (5 0 0 0 0 0 0 0 2)
+                 (0 0 7 2 0 6 9 0 0)
+                 (0 4 0 5 0 8 0 7 0)))
   (print-board grid)
-  (princ "Enter column : ")
-  (setq col (gethash (read) *hash-let-to-num* ))
-  ;;rajouter test validité de la colonne
-  (princ "Enter row : ")
-  (setq row (read))
-  ;;rajouter test validité de la ligne
-  (if (= 0 (aref grid row col))
-      (progn (princ "Enter value : ")
-       (setq val (read))
-       (let (g-temp)
-	 (setf g-temp grid)
-	 (setf (aref g-temp row col) val)
-	 (if (/= 0 (check-grid g-temp))
-	     (setf (aref grid row col) val)
-	     (format t "Erreur, vous ne pouvez pas jouer cela ici"))
-       )
-      )
-      (princ "Erreur, Case non vide")
-  )
-  ;;definir condition d'arret
   (game-loop)
 )
 
-(defun check-grid(g)
+(defun check-grid (g row col val)
+
   ;;check lines
-  (let ((tempList '())
-	(nb 0))
+  (dotimes (r grid-size)
+    (setq tmpList '())
+    (setq nb 0)
+    (dotimes (c grid-size)
+      (if (/= 0 (aref g r c))
+	  (push (aref g r c) tmpList))
+    )
+    (setq nb (length tmpList))
+    (if (> nb (length (remove-duplicates tmpList)))
+        (progn
+          (format t "Erreur, vous ne pouvez pas jouer cela ici")
+          (return-from check-grid 0)
+        )
+    )
+    
+  )
+  ;;check columns
+  (dotimes (c grid-size)
+    (setq tmpList '())
+    (setq nb 0)
     (dotimes (r grid-size)
-      (dotimes (c grid-size)
-	(if (/= 0 (aref g r c ))
-	    (push (aref g r c) tempList))))
-    (setq nb (length tempList))
-    (setq tempList (remove-duplicates tempList))
-    (if (> nb (length tempList))
-	0
-	1
+      (if (/= 0 (aref g r c))
+	  (push (aref g r c) tmpList))
+    )
+    (setq nb (length tmpList))
+    (if (> nb (length (remove-duplicates tmpList)))
+        (progn
+          (format t "Erreur, vous ne pouvez pas jouer cela ici")
+          (return-from check-grid 0)
+        )
     )
   )
-      
-      
+ 
+  ;;check squares
+  (let ((offset_r 0)
+       (offset_c 0))
+       (dotimes (x 3)
+	 (dotimes (y 3)
+	   (setq tmpList '())
+	   (setq nb 0)
+	   (dotimes (r 3)
+	     (dotimes (c 3)
+	       (if (/= 0 (aref g (+ r offset_r) (+ c offset_c)))
+		   (push (aref g (+ r offset_r) (+ c offset_c)) tmpList)
+	       )
+	     )
+	   )
+	   (setq nb (length tmpList))
+	   (if (> nb (length (remove-duplicates tmpList)))
+	       (progn
+		 (format t "Erreur, vous ne pouvez pas jouer cela ici")
+		 (return-from check-grid 0)
+	       )
+	   )
+	   (setq offset_c (+ 3 offset_c))
+	 )
+	 (setq offset_c 0)
+	 (setq offset_r (+ 3 offset_r))
+       )
+  )
+  (setf (aref grid row col) val)
+
 )
+
 
 (defun print-board (g)
   (format t "~%   | A | B | C | D | E | F | G | H | I |")
